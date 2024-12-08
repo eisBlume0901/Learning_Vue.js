7951f2fb985328178b42b13e0b14dcc009411d94
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-12-08 21:15:04 +0800

    Added HomeCards layout

---
+++ Learning_Vue.js/src/components/HomeCards.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="py-4">
+    <div class="container-xl lg:container m-auto">
+     <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+       <div class="bg-pale-green p-6 rounded-lg shadow-md">
+         <h2 class="text-2xl font-bold text-white">For Herbal Enthusiasts</h2>
+         <p class="mt-2 mb-4 text-white">
+           Find the perfect herbal remedy for your needs. Our shop offers a wide selection of herbal potions and remedies that are carefully crafted to help you heal naturally.
+         </p>
+         <a href="" class="inline-block bg-bark text-white rounded-lg px-4 py-2 hover:bg-sepia">Browse Potions</a>
+       </div>
+
+       <div class="bg-sepia p-6 rounded-lg shadow-md">
+         <h2 class="text-2xl font-bold text-white">For Health Practitioners</h2>
+         <p class="mt-2 mb-4 text-white">
+           Take your practice to the next level with our premium selection of herbal potions and remedies. Our products are made with the highest quality ingredients to ensure the best results for your clients.
+         </p>
+         <a href="" class="inline-block bg-pale-green text-white rounded-lg px-4 py-2 hover:bg-white hover:text-bark">Browse Potions</a>
+       </div>
+
+     </div>
+    </div>
+  </section>
+</template>
