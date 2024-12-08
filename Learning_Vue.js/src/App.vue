5535f1d017f7f35633730505bdf581bdf5cc5fc8
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-12-08 20:43:27 +0800

    Added Navbar layout

--- Learning_Vue.js/src/App.vue
+++ Learning_Vue.js/src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
+import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
+  <Navbar />
   <h1 class="text-bark font-bold">Eisblume's Apothecary Shop</h1>
 </template>
---
+++ Learning_Vue.js/src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+ <nav class="bg-pale-green">
+   <div class="mx-auto max-w-7xl">
+     <div class="flex h-20 items-center justify-between">
+       <div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+         <a href=""><span class="text-white text-2xl font-bold">Eisblume's Apothecary Shop</span></a>
+       </div>
+       <div class="md:ml-auto">
+         <div class="flex space-x-2">
+           <a href="" class="text-white bg-bark hover:bg-sepia  rounded-md px-3 py-2">
+             Home
+           </a>
+           <a href="" class="text-white bg-bark hover:bg-sepia  rounded-md px-3 py-2">
+             Orders
+           </a>
+           <a href="" class="text-white bg-bark hover:bg-sepia  rounded-md px-3 py-2">
+             Add Order
+           </a>
+         </div>
+
+       </div>
+     </div>
+
+   </div>
+ </nav>
+
+</template>
