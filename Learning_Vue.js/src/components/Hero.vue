7dcfffe02c7747485cc4417baba7e5d7daecd974
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-12-08 20:59:22 +0800

    Added defineProps in Hero for dynamic rendering

--- Learning_Vue.js/src/components/Hero.vue
+++ Learning_Vue.js/src/components/Hero.vue
@@ -1,4 +1,16 @@
 <script setup>
+import {defineProps} from 'vue';
+
+defineProps({
+  title: {
+    type: String,
+    default: 'Find Your Natural Remedy'
+  },
+  subtitle: {
+    type: String,
+    default: 'Discover the healing power of nature with our carefully crafted herbal potions and remedies'
+  }
+})
 </script>
 
 <template>
@@ -6,10 +18,11 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
       <div class="text-center">
         <h1 class="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
-          Find Your Natural Remedy
+        {{ title }}
         </h1>
         <p class="my-4 text-xl text-white">
-          Discover the healing power of nature with our carefully crafted herbal potions and remedies        </p>
+        {{ subtitle }}
+        </p>
       </div>
     </div>
   </section>
