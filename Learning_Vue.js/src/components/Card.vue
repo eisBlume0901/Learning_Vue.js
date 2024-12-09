1a763a2dfd9021c95578e9e46b17f17265be605d
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-12-09 20:58:53 +0800

    Created Card Layout for Homecards for reusability. Learned about class binding for dynamic css styling

---
+++ Learning_Vue.js/src/components/Card.vue
@@ -0,0 +1,44 @@
+<script setup>
+import {defineProps} from 'vue';
+
+defineProps({
+  headline: {
+    type: String,
+    default: 'For Herbal Enthusiasts'
+  },
+  description: {
+    type: String,
+    default: 'Find the perfect herbal remedy for your needs. Our shop offers a wide selection of herbal potions and remedies that are carefully crafted to help you heal naturally.'
+  },
+  cardColor: {
+    type: String,
+    default: 'bg-pale-green'
+  },
+  buttonText: {
+    type: String,
+    default: 'Browse Potions'
+  },
+  buttonColor: {
+    type: String,
+    default: 'bg-bark'
+  },
+  buttonHoverColor: {
+    type: String,
+    default: 'hover:bg-sepia'
+  },
+  textHoverColor: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <div :class="`${cardColor} p-6 rounded-lg shadow-md`">
+    <h2 class="text-2xl font-bold text-white">{{ headline }}</h2>
+    <p class="mt-2 mb-4 text-white">
+      {{ description }}
+    </p>
+    <a href="" :class="`inline-block ${buttonColor} text-white rounded-lg px-4 py-2 ${buttonHoverColor} ${textHoverColor}`">{{ buttonText }}</a>
+  </div>
+</template>
--- Learning_Vue.js/src/components/HomeCards.vue
+++ Learning_Vue.js/src/components/HomeCards.vue
@@ -1,26 +1,25 @@
 <script setup>
 
+import Card from "@/components/Card.vue";
+
+
 </script>
 
 <template>
   <section class="py-4">
     <div class="container-xl lg:container m-auto">
      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-       <div class="bg-pale-green p-6 rounded-lg shadow-md">
-         <h2 class="text-2xl font-bold text-white">For Herbal Enthusiasts</h2>
-         <p class="mt-2 mb-4 text-white">
-           Find the perfect herbal remedy for your needs. Our shop offers a wide selection of herbal potions and remedies that are carefully crafted to help you heal naturally.
-         </p>
-         <a href="" class="inline-block bg-bark text-white rounded-lg px-4 py-2 hover:bg-sepia">Browse Potions</a>
-       </div>
+       <Card/>
 
-       <div class="bg-sepia p-6 rounded-lg shadow-md">
-         <h2 class="text-2xl font-bold text-white">For Apothecaries</h2>
-         <p class="mt-2 mb-4 text-white">
-           List your herbal potions and remedies in our shop. We offer a platform for you to showcase your products and reach a wider audience of herbal enthusiasts.
-         </p>
-         <a href="" class="inline-block bg-pale-green text-white rounded-lg px-4 py-2 hover:bg-white hover:text-bark">Browse Potions</a>
-       </div>
+       <Card headline="For Apothecaries"
+             description="List your herbal potions and remedies in our shop. We offer a platform for you to showcase your products and reach a wider audience of herbal enthusiasts."
+             cardColor="bg-sepia"
+             buttonText="List Your Products"
+             buttonColor="bg-pale-green"
+             buttonHoverColor="hover:bg-white"
+             textHoverColor="hover:text-sepia"
+       />
+
 
      </div>
     </div>
